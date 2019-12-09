98c33bbb40da2561c649c225c11a6e210a49d9bf
Yuriy <befree312@gmail.com>
2019-12-09 22:28:15 +0200

    task1

---
+++ task1/script.js
@@ -0,0 +1,23 @@
+var money = prompt("Ваш бюджет на месяц?", "");
+var time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+
+let answer1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    answer2 = prompt("Во сколько обойдется?", ''),
+    answer3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    answer4 = prompt("Во сколько обойдется?", '');
+
+appData.expenses[answer1] = answer2;
+appData.expenses[answer3] = answer4;
+
+alert(appData.budget / 30);
+console.log(appData);
