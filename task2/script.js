a0e41d184d5817bdb0152e6f0b8ae9f87693b94a
Yuriy <befree312@gmail.com>
2019-12-10 04:37:10 +0200

    task2 complete

--- task2/script.js
+++ task2/script.js
@@ -11,6 +11,36 @@ let appData = {
     savings: false
 };
 
+// let i = 2;
+
+// while (i > 0) {
+//     let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+//         b = prompt("Во сколько обойдется?", '');
+
+//     if ( (typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b)) != null 
+//         && a != '' && b !='' && a.length < 50) { 
+//         console.log("done");
+//         appData.expenses[a] = b;
+//         i--;
+//     } else {
+//         i--;
+//     }
+// };
+
+// let i = 0;
+
+// do {
+//     let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+//         b = prompt("Во сколько обойдется?", '');
+//     if ( (typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b)) != null 
+//         && a != '' && b !='' && a.length < 50) { 
+//         console.log("done");
+//         appData.expenses[a] = b;
+//         i++;
+//     } else {
+//         i++;
+//     }
+// } while (i < 2);
 
 
 for (let i = 0; i < 2; i++) {
@@ -26,6 +56,7 @@ for (let i = 0; i < 2; i++) {
     } 
     
 };
+
 
 appData.moneyPerDay = appData.budget / 30;
 
